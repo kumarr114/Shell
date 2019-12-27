07df171d06e6eebe63a5d58b526ba041642a171c
Rajat Kumar <47738784+kumarr114@users.noreply.github.com>
2019-12-27 20:00:40 +0530

    Update platform_service_check.sh

--- platform_service_check.sh
+++ platform_service_check.sh
@@ -1,4 +1,3 @@
-
 #Script developed to troubleshoot the Platform Service Related Issues on VxRail
 
 #KB - 517433
@@ -12,8 +11,8 @@ echo "This script is trying to automate the steps to isolate issue in KB 517433"
 script_version="0.1"
 run_time=$(date)
 
-echo "/// Date / Time:    "$run_time
-echo "/// Script version: "$script_version
+echo " Current Date / Time:    "$run_time
+echo " Script version: "$script_version
 
 # Check if Platform service is initialized
 
@@ -26,14 +25,21 @@ then
 	echo "Platform service not initialized, will try to restart Platform Service and check again"
 	/etc/init.d/vxrail-pservice restart
 	if_initialized_again=$(esxcli vxrail agent get | awk '/false/ {print $2}')
-	if [ -n "$is_initialized_again" ]
+	if [ -n "$if_initialized_again" ]
     then
 	    echo "Platform service not initialized, Check KB 517433"
 	fi
+	
+else
+    echo "Platform service is initialized"
 fi
 
+echo "********************************CHECK 2***************************************"
 
-firmware_list=$(esxcli vxrail firmware list | grep -i bmc)
+#During Upgrade we use this to get current firmware version and schedule upgrade after comparing the value with Upgrade Bundle files 
+
+
+firmware_list=$(esxcli vxrail firmware list | grep -i idrac)
 if [ -n "$firmware_list" ]
 then
     echo "Firmware is listed as expected, good ["$firmware_list"]"
@@ -44,9 +50,9 @@ fi
 echo "If the Agent is still listed as false,check logs located at /var/log/platform_svc.log"
 
 
-#Checking that all users are configured as expcted
+#Checking that all users are configured as expected
 
-echo "********************************CHECK 2***************************************"
+echo "********************************CHECK 3***************************************"
 
 /opt/vxrail/tools/ipmitool user list 1 | awk '{if (NR==3) print $0; if (NR==16) print $0;if (NR==17) print $0}'
 
@@ -58,12 +64,32 @@ echo "If the above output contains vxpsvc & PTAdmin and in enabled state then it
 
 #Check of iSM is working correctly with vUSB IP
 
-echo "********************************CHECK 3***************************************"
+echo "********************************CHECK 4***************************************"
 ping 169.254.0.1
 
 echo "******************************************************************************"
 
-echo "If the ping is unsuccessfull , then check the iDRAC vswitch configuration"
+echo "If the ping is unsuccessful , then check the iDRAC vswitch configuration"
 
 
+echo "********************************CHECK 5***************************************"
+
+#This is to check if PTAgent is able to listen.
+
+pt_initialized=$(esxcli network ip connection list | grep LISTEN | grep Dell | awk '{print $4}')
+
+if [ -n "$pt_initialized" ]
+then
+    echo "This is working and PTAgent is able to listen : $pt_initialized"
+	
+else
+    echo "PTAgent is not able to listen will restart Respective services & reset iDRAC"
+	/etc/init.d/DellPTAgent restart
+	/etc/init.d/dcism-netmon-watchdog restart
+	/opt/dell/DellPTAgent/bin/ipmitool_static mc reset cold
+    echo "Checking the rest_ip config"
+	less /scratch/dell/config/PTAgent.config | grep -i rest_ip
+fi
+
+echo "Check PTAgent KB - 498849 if the above listening is still returning as Null.
 
